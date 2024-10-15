a62c9dbd7cbc77641a4e38a3aa88a95879e27e97
masato0117 <satomasato0117@icloud.com>
2024-10-15 16:40:25 +0900

    field.py

--- field.py
+++ field.py
@@ -29,4 +29,45 @@ class Field:
             field_size (int): フィールドサイズ
         """
         pass
+
+    def field_update(self) -> list[list[str]]:
+        """
+        プレイヤー，敵，物体の位置を参照して，フィールドを更新する関数
+
+        Returns:
+            list[list[str]]: 更新されたフィールド
+
+        Examples:
+        
+        """
+
+        pass
+
+    def field_display(self):
+        """
+        フィールドを表示する関数
+
+        Example:
+
+        """
+
+        pass
+
+    def collision_detection(self):
+        """
+        プレイヤー，敵，物体の位置が重なっているか判定する関数
+        
+        Args:
+            target (Item): アイテム1
+            items (list[Item]): アイテム2
+
+        Returns:
+            Item | None: 重なっているアイテムがあればそのアイテム，なければNone
+
+        Examples:
+
+        """
+
+        pass 
+
 >>>>>>> field
